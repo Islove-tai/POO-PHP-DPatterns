cf6fa129182108d345e791e5803b37fc02dcb89c
Islove-tai <aimeisraelislove@gmail.com>
2022-06-16 19:57:53 +0100

    test 2

--- blog/config/config.php
+++ blog/config/config.php
@@ -2,12 +2,9 @@
 
 return [
 
-  "db_user" => "root",
-  
+  "db_user" => "root",  
   "db_pass" => "root", 
-
-  "db_host" => "localhost",  
-
+  "db_host" => "localhost",
   "db_name" => "blog_db"
 
 
